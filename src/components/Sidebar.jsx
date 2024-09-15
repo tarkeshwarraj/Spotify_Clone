1440f7c02683bb31ef693612b811de0d42b04a28
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-09-15 08:25:10 +0530

    home navigate

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+
+    const navigate = useNavigate();
   return (
     <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
         {/* 1st part Div */}
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
         {" "}
         {/* This is flex column so inside this div all div be in straight down */}
-        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+        <div onClick={()=>navigate('/')} className="flex items-center gap-3 pl-8 cursor-pointer">
           <img className="w-6" src={assets.home_icon} alt="" />
           <p className="font-bold">Home</p>
         </div>
